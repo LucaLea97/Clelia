8e70d4cb48859c4f8184bc586cb8c5a254c8f450
LucaLea97 <luca.domenichelli@lea-renergy.com>
2025-01-10 16:01:47 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React, { Component } from 'react';
+import Amplify, { PubSub } from 'aws-amplify';
+import { AWSIoTProvider } from '@aws-amplify/pubsub/lib/Providers';
 
 function App() {
   const [count, setCount] = useState(0)
