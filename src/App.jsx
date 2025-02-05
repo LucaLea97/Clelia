54f54f0620b057ab8b99ea364e8e7c474fbccc2c
LucaLea97 <luca.domenichelli@lea-renergy.com>
2025-02-05 15:36:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 //  const [count, setCount] = useState(0)
 
   return (
+    <>
     <body>
     <header>Clelia</header>
     <main>
@@ -19,7 +20,7 @@ function App() {
     </aside>
     </main>
     </body>
-    
+    </>
 
   )
 }
