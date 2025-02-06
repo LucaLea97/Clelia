e35d7f9863c6bed090b47e14e4ab497e23da26ce
gianluca strada <gianlucastrada@Host-003.homenet.telecomitalia.it>
2025-02-06 17:42:56 +0100

    poc supabase - WIP

---
+++ src/components/loginPage.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import supabase from "../utils/supabase.js";
+import {Auth} from "@supabase/auth-ui-react";
+
+function LoginPage() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+            if (session) {
+                navigate('/'); // Redirect to home page on success
+            }
+        });
+        return () => authListener?.subscription.unsubscribe();
+    }, [navigate]);
+
+    return (
+        <div>
+            <h2>Login or Sign Up</h2>
+            <Auth
+                supabaseClient={supabase}
+                providers={['google', 'github']}
+                appearance={{ theme: ThemeSupa }}
+            />
+        </div>
+    );
+}
+
+export default LoginPage;
