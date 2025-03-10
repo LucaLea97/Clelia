223bef0b7ba12141056c9fa245b8595267c61731
LucaLea97 <luca.domenichelli@lea-renergy.com>
2025-03-10 14:22:15 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import CleliaAppLogo from './assets/CleliaAppLogo.png'
+import CleliaAppLogo from '../assets/CleliaAppLogo.png'
 import './App.css'
 
 
@@ -14,7 +14,7 @@ function App() {
     <main>
     <button> Lock </button>
     <button> Unlock </button>
-    <img src="assets/CleliaAppLogo.png"/>
+    <img src={CleliaAppLogo}/>
     <aside>
     <button> Map </button>
     <button> Controls </button>
