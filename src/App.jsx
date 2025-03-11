d1eae6047919ec5c91ff4ded9594a9b0af99c22e
LucaLea97 <luca.domenichelli@lea-renergy.com>
2025-03-11 16:12:52 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,8 @@ import './App.css'
 
 function App() {
 return (
-    <header>
-    <h1>Clelia</h1>
-    </header>
-    <main>
     <button> Lock </button>
-    <aside>
-    <button> Map </button>
-    </aside>
-    </main>
-    <footer>Lea Renergy s.r.l.</footer>
-  );
+  )
 }
 
 export default App
