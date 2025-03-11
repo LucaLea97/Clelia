9effa1061743810f030579918ef01686bb96006f
LucaLea97 <luca.domenichelli@lea-renergy.com>
2025-03-11 11:12:53 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,9 @@
+import React from "react";
+import CleliaAppLogo from './assets/CleliaAppLogo.png'
 
+function Navbar(){
+  return(
+    <div className="navbar">
+     <div className="leftSide"></div>
+     <div className="rightSide"></div>
+    </div>
